fd68b62578821f550f524f8dc70f9c81f3a41a6d
Unknown <tanabebr@gmail.com>
2018-11-27 21:42:59 -0200

    Implementação da Tela Pós Pesquisa

---
+++ resources/views/SearchBrowse.php
@@ -0,0 +1,158 @@
+@include('util.header')
+<!--End header include -->
+
+
+
+      <div id="pageContainer">
+         <div id="leftColumn">
+            <!--Begin menu include -->
+
+<div class="menuContainer">
+   <div class="menuSearch" >
+      <div class="menuHead">
+         Search
+      </div>
+
+      <div class="menuBorder">
+         <form action="SearchBrowse.php" >
+            <input type="text" name="search" autofocus />
+            <input type="submit" value="Search" class="button fullWidth" />
+         </form>
+      </div>
+   </div>
+
+   <nav class="IsDesktop">
+      <div class="menuHead">
+         Browse
+      </div>
+
+      <div class="menuBorder">
+
+         <a href='SearchBrowse.php?catID=5&catName=ASP.NET' class='menuitem'>ASP.NET</a><br />
+ <a href='SearchBrowse.php?catID=9&catName=JavaScript' class='menuitem'>JavaScript</a><br />
+ <a href='SearchBrowse.php?catID=2&catName=MySQL' class='menuitem'>MySQL</a><br />
+ <a href='SearchBrowse.php?catID=1&catName=PHP' class='menuitem'>PHP</a><br />
+ <a href='SearchBrowse.php?catID=6&catName=Regular%20Expressions' class='menuitem'>Regular Expressions</a><br />
+ <a href='SearchBrowse.php?catID=4&catName=SQL' class='menuitem'>SQL</a><br />
+ <a href='SearchBrowse.php?catID=3&catName=Web%20Usability' class='menuitem'>Web Usability</a><br />
+ 
+      </div>
+   </nav>
+</div>
+
+<!--End menu include -->
+         </div>
+         <!-- start dynamic content -->
+         <div id="pageContent">
+            
+<!-- start page content *************** -->
+
+
+<div class="pageTitle2">
+8 books contain <font color='#CC0000'>"php"</font>
+</div><br />               <div class="bookSimple">                
+
+                  <a class="booktitle" href="ProductPage.php?isbn=1491918667">  Learning PHP, MySQL & JavaScript: With jQuery, CSS & HTML5 </a>
+                  <br />
+                  <span class="authors">by <a href='SearchBrowse.php?search=Nixon'>Robin Nixon</a></span><br />
+                  <a href="ProductPage.php?isbn=1491918667">
+                      <img class="Book"
+                      src="/sandvig/mis314/assignments/bookstore/bookimages/1491918667.01.THUMBZZZ.jpg">
+                  </a>
+
+                      <p>         This book is for people who wish to learn how to create effective and dynamic websites. This may include webmasters or graphic designers who are already creating static websites but wish to take their skills to the next level, as well				  <a href="ProductPage.php?isbn=1491918667">more...</a>
+               </div>
+               <div class="bookSimple">                
+
+                  <a class="booktitle" href="ProductPage.php?isbn=1590595521"> Beginning PHP and MySQL 5 </a>
+                  <br />
+                  <span class="authors">by <a href='SearchBrowse.php?search=Gilmore'>Jason Gilmore</a></span><br />
+                  <a href="ProductPage.php?isbn=1590595521">
+                      <img class="Book"
+                      src="/sandvig/mis314/assignments/bookstore/bookimages/1590595521.01.THUMBZZZ.jpg">
+                  </a>
+
+                  <p> <em>Beginning PHP 5 and MYSQL: From Novice to Professional</em> offers a comprehensive introduction to two of the most popular Web  application building technologies on the planet: the scripting language  PHP and the MySQL database server. This b				  <a href="ProductPage.php?isbn=1590595521">more...</a>
+               </div>
+               <div class="bookSimple">                
+
+                  <a class="booktitle" href="ProductPage.php?isbn=0596005601"> Learning PHP 5 </a>
+                  <br />
+                  <span class="authors">by <a href='SearchBrowse.php?search=Sklar'>David Sklar</a></span><br />
+                  <a href="ProductPage.php?isbn=0596005601">
+                      <img class="Book"
+                      src="/sandvig/mis314/assignments/bookstore/bookimages/0596005601.01.THUMBZZZ.jpg">
+                  </a>
+
+                  <p>PHP has gained a following among non-technical web designers who need  to add interactive aspects to their sites. Offering a gentle learning  curve, PHP is an accessible yet powerful language for creating dynamic  web pages. As its popularity has 				  <a href="ProductPage.php?isbn=0596005601">more...</a>
+               </div>
+               <div class="bookSimple">                
+
+                  <a class="booktitle" href="ProductPage.php?isbn=0596101104"> Learning PHP and MySQL </a>
+                  <br />
+                  <span class="authors">by <a href='SearchBrowse.php?search=Davis'>Michele E. Davis</a>, <a href='SearchBrowse.php?search=Phillips'>John A. Phillips</a></span><br />
+                  <a href="ProductPage.php?isbn=0596101104">
+                      <img class="Book"
+                      src="/sandvig/mis314/assignments/bookstore/bookimages/0596101104.01.THUMBZZZ.jpg">
+                  </a>
+
+                  <p> The PHP scripting language and MySQL open source database are quite  effective independently, but together they make a simply unbeatable  team. When working hand-in-hand, they serve as the standard for the  rapid development of dynamic, database-				  <a href="ProductPage.php?isbn=0596101104">more...</a>
+               </div>
+               <div class="bookSimple">                
+
+                  <a class="booktitle" href="ProductPage.php?isbn=0596528124"> Mastering Regular Expressions   </a>
+                  <br />
+                  <span class="authors">by <a href='SearchBrowse.php?search=Friedl'>Jeffrey Friedl</a></span><br />
+                  <a href="ProductPage.php?isbn=0596528124">
+                      <img class="Book"
+                      src="/sandvig/mis314/assignments/bookstore/bookimages/0596528124.01.THUMBZZZ.jpg">
+                  </a>
+
+                  <p>Regular expressions are an extremely powerful tool for manipulating  text and data. They are now standard features in a wide range of  languages and popular tools, including Perl, Python, Ruby, Java, VB.NET  and C# (and any language using the .NET				  <a href="ProductPage.php?isbn=0596528124">more...</a>
+               </div>
+               <div class="bookSimple">                
+
+                  <a class="booktitle" href="ProductPage.php?isbn=0672326728"> PHP & MySQL Web Development </a>
+                  <br />
+                  <span class="authors">by <a href='SearchBrowse.php?search=Thomson'>Laura Thomson</a>, <a href='SearchBrowse.php?search=Welling'>Luke Welling</a></span><br />
+                  <a href="ProductPage.php?isbn=0672326728">
+                      <img class="Book"
+                      src="/sandvig/mis314/assignments/bookstore/bookimages/0672326728.01.THUMBZZZ.jpg">
+                  </a>
+
+                  <p>The PHP server-side scripting language and the MySQL database  management system (DBMS) make a potent pair. Both are open-source  products--free of charge for most purposes--remarkably strong, and  capable of handling all but the most enormous tra				  <a href="ProductPage.php?isbn=0672326728">more...</a>
+               </div>
+               <div class="bookSimple">                
+
+                  <a class="booktitle" href="ProductPage.php?isbn=0596006810"> Programming PHP </a>
+                  <br />
+                  <span class="authors">by <a href='SearchBrowse.php?search=Lerdorf'>Rasmus Lerdorf</a>, <a href='SearchBrowse.php?search=MacIntyre'>Peter MacIntyre</a>, <a href='SearchBrowse.php?search=Tatroe'>Kevin Tatroe</a></span><br />
+                  <a href="ProductPage.php?isbn=0596006810">
+                      <img class="Book"
+                      src="/sandvig/mis314/assignments/bookstore/bookimages/0596006810.01.THUMBZZZ.jpg">
+                  </a>
+
+                  <p><em>Programming PHP</em>, 2nd Edition, is the authoritative guide to  PHP 5 and is filled with the unique knowledge of the creator of PHP  (Rasmus Lerdorf) and other PHP experts. When it comes to creating  websites, the PHP scripting language is t				  <a href="ProductPage.php?isbn=0596006810">more...</a>
+               </div>
+               <div class="bookSimple">                
+
+                  <a class="booktitle" href="ProductPage.php?isbn=0596005431"> Web Database Applications with PHP & MySQL </a>
+                  <br />
+                  <span class="authors">by <a href='SearchBrowse.php?search=Lane'>David Lane</a></span><br />
+                  <a href="ProductPage.php?isbn=0596005431">
+                      <img class="Book"
+                      src="/sandvig/mis314/assignments/bookstore/bookimages/0596005431.01.THUMBZZZ.jpg">
+                  </a>
+
+                  <p> There are many reasons for serving up dynamic content from a web  site: to offer an online shopping site, create customized information  pages for users, or just manage a large volume of content through a  database. Anyone with a modest knowledge				  <a href="ProductPage.php?isbn=0596005431">more...</a>
+               </div>
+      
+<!-- end page content *************** -->
+
+         </div> 
+         <!-- end dynamic content -->
+
+         <!--Begin footer include -->
+@include('util.footer')
+
+
