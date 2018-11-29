f4999fce980f58249ae81783d9eab81ca6363395
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2018-11-29 17:10:46 -0200

    Quantidades no histórico

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -4,10 +4,16 @@
     <div id="pageContent" style="background-color: #fff; width: 97.5%">
 
         @if(isset($customer))
-           <center> <a href="{{ route('logout') }}">Logout</a></center>
+            @php($total = 0)
+            @php($qty = 0)
+            <center><a href="{{ route('logout') }}">Logout</a></center>
             @foreach($customer->orders as $order)
+                @php($totalOrder = 0)
                 <div style="border: 2px solid red; margin: 20px 10px; padding: 10px;">
                     @foreach($order->items as $item)
+                        @php($totalOrder += $item->total)
+                        @php($total += $item->total)
+                        @php($qty += $item->qty)
                         <div class="bookSimple">
                             <a class="booktitle" href="{{route('exibirLivro', ['ISBN' => $item->book->ISBN])}}"> {{ $item->book->title }} </a> <br/>
 
@@ -17,28 +23,30 @@
                             </a>
 
                             <p>{!! str_limit($item->book->description, 200, '...') !!} <a href="{{route('exibirLivro', ['ISBN' => $item->book->ISBN])}}">more...</a>
-                            <br><br>
+                                <br><br>
                             <div>
-                                <span class="priceLabel"><b>Qty:</b></span>
-                                <span class="bookPriceB">{{$item->qty}}</span>
-                                <br>
                                 <span class="priceLabel"><b>Price:</b></span>
-                                <span class="bookPriceB">${{$item->price}}</span>
-                                <br>
-                                <span class="priceLabel"><b>Price Total:</b></span>
+                                <span class="bookPriceB">${{$item->price}}</span> -
+                                <span class="priceLabel"><b>Qty:</b></span>
+                                <span class="bookPriceB">{{$item->qty}}</span> -
+                                <span class="priceLabel"><b>Total price:</b></span>
                                 <span class="bookPriceB">${{$item->total}}</span>
                                 <br>
                             </div>
                         </div>
                     @endforeach
+
+                    <hr>
+                    <span class="priceLabel"><b>Total price:</b></span>
+                    <span class="bookPriceB">${{ $totalOrder }}</span>
                 </div>
             @endforeach
             <div id="pageContent" style="background-color: #fff; width: 97.5%">
                 <span class="priceLabel"><b>Qty Total:</b></span>
-                <span class="bookPriceB">0</span>
+                <span class="bookPriceB">{{ $qty }}</span>
                 <br>
                 <span class="priceLabel"><b>Price Total:</b></span>
-                <span class="bookPriceB">$0</span>
+                <span class="bookPriceB">${{ $total }}</span>
                 <br>
             </div>
         @else
