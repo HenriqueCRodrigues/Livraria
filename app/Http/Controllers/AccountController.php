dac7cf22d4e0229e89b1442c749154e881df1cf8
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2018-11-29 16:27:13 -0200

    Login / Logout

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\BookCategory;
 use App\Models\BookCustomer;
+use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
@@ -23,5 +24,20 @@ class AccountController extends Controller
         $categories = BookCategory::orderBy('CategoryName', 'asc')->get();
 
         return view('about', compact('categories'));
+    }
+
+    public function login(Request $request)
+    {
+        $customer = BookCustomer::where('email', '=', $request->get('email'))->first();
+        setcookie('customer', json_encode($customer), time() + 60 * 24 * 3, '/', NULL, 0);   //3 Dias
+
+        return redirect()->route('conta');
+    }
+
+    public function logout()
+    {
+        setcookie('customer', null, time() + 60 * 24 * 3, '/', NULL, 0);   //3 Dias
+
+        return redirect()->route('conta');
     }
 }
